baea7fb1fbe7d0e4c4fd4a5b596674f0392fced2
joelflowstack <joelflowstack@gmail.com>
2026-06-12 14:36:35 +0100

    Update Footer.tsx

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,36 @@
+export default function Footer() {
+
+  return (
+
+    <footer
+      className="
+      border-t
+      border-border
+      py-12
+      px-6
+      "
+    >
+
+      <div
+        className="
+        max-w-7xl
+        mx-auto
+        flex
+        justify-between
+        "
+      >
+
+        <div>
+          AI//AGENCY
+        </div>
+
+        <div>
+          © 2026
+        </div>
+
+      </div>
+
+    </footer>
+
+  );
+}
