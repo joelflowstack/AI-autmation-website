79b728513239210f252e0eb9a52dce854c03348c
joelflowstack <joelflowstack@gmail.com>
2026-06-12 14:39:39 +0100

    Update CircuitBackground.tsx

---
+++ src/components/effects/CircuitBackground.tsx
@@ -0,0 +1,16 @@
+export default function CircuitBackground() {
+
+  return (
+
+    <div
+      className="
+      absolute
+      inset-0
+      opacity-30
+      pointer-events-none
+      cyber-grid
+      "
+    />
+
+  );
+}
